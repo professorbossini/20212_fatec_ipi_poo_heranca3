f7ae9058a2502dd28b51fe3b713dfdea7a7dd2b6
Rodrigo Bossini <professorbossini@gmail.com>
2021-09-16 18:12:15 -0300

    Classes Pessoa, Professores e Alunos criadas. Ainda sem encapsulamento

---
+++ ProfessorPesquisador.java
@@ -0,0 +1,5 @@
+public class ProfessorPesquisador extends Professor{
+    public void pesquisar(){
+        System.out.printf ("Professor chamado %s de matricula %d esta pesquisando", nome, matricula);
+    }
+}
